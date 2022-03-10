2ce28e20d1d40b71732d895c1c8c3f563cbc2412
opeyemi <opelium@gmail.com>
2022-03-10 17:58:10 +0100

    migrations

--- To_Do/settings.py
+++ To_Do/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-iv!ejd9a$+*^e_w2yv(*p1#4v=5k0)!7wqarqx2+f$#56j7h_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['opeyemitodo.herokuapp.com', '127.0.0.1', '10.0.12.195']
+ALLOWED_HOSTS = ['opeyemitodo.herokuapp.com', '127.0.0.1']
 
 #10.0.12.195:80
 # Application definition
